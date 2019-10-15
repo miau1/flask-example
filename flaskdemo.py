b1536e87f90702cbbcf17d845ac0c1050b924168
miau1 <aulamo.mikko@gmail.com>
2019-10-15 17:10:47 +0300

    simplify example

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,38 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 #Initialize Flask instance
 app = Flask(__name__)
 
 example_data = [
-    {"name": "Cat sleeping on a bed", "source": "cat.jpg"},
-    {"name": "Misty forest", "source": "forest.jpg"},
-    {"name": "Bonfire burning", "source": "fire.jpg"},
-    {"name": "Old library", "source": "library.jpg"},
-    {"name": "Sliced orange", "source": "orange.jpg"}
+    {'name': 'Cat sleeping on a bed', 'source': 'cat.jpg'},
+    {'name': 'Misty forest', 'source': 'forest.jpg'},
+    {'name': 'Bonfire burning', 'source': 'fire.jpg'},
+    {'name': 'Old library', 'source': 'library.jpg'},
+    {'name': 'Sliced orange', 'source': 'orange.jpg'}
 ]
 
-#Use "query" variable from the URL. If no variable is given,
-#use empty string instead. GET and POST methods are allowed.
-@app.route("/search", defaults={"query": ""}, methods=["GET", "POST"])
-@app.route("/search/<query>", methods=["GET", "POST"])
-def search(query):
+#Function search() is associated with the address base URL + "/search"
+@app.route('/search')
+def search():
 
-    if request.method == "POST":
-        #Get query from the POST form.
-        query = request.form["query"]
-        
-        #Redirect to the same page with the query in the url.
-        #ALWAYS REDIRECT AFTER POSTING!
-        return redirect(url_for("search", query=query))
+    #Get query from URL variable
+    query = request.args.get('query')
 
+    #Initialize list of matches
     matches = []
 
-    #If an entry name contains the query, add the entry to matches.
-    if query != "":
+    #If query exists (i.e. is not None)
+    if query:
+        #Look at each entry in the example data
         for entry in example_data:
-            if query.lower() in entry["name"].lower():
+            #If an entry name contains the query, add the entry to matches
+            if query.lower() in entry['name'].lower():
                 matches.append(entry)
- 
-    #Render index.html with matches variable. 
-    return render_template("index.html", matches=matches)
+
+    #Render index.html with matches variable
+    return render_template('index.html', matches=matches)
 
